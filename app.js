6d9ae4a78457bd0b8ced9257c441ce79181d150c
Ali Malek <ali.malek.71@gmail.com>
2017-02-14 13:49:54 +0330

    add while loop

---
+++ app.js
@@ -0,0 +1,20 @@
+var bluebirdPromise = require('bluebird')
+
+var promiseWhile = function (condition, action) {
+    var resolver = bluebirdPromise.defer();
+
+    var loop = function () {
+        if (!condition()) return resolver.resolve();
+        return bluebirdPromise.cast(action())
+            .then(loop)
+            .catch(resolver.reject)
+    };
+
+    process.nextTick(loop);
+
+    return resolver.promise;
+}
+
+module.exports = {
+    while: promiseWhile
+}
